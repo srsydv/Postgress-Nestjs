f6c323d0f380d33494d9e1fd2ba72e53764f2645
srsydv <shrishofficial18@gmail.com>
2026-05-29 20:40:43 +0530

    Add user creation functionality with DTO validation; include class-validator and update user module to use TypeORM for user entity management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { User } from './entity/user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post('create')
+    async createUser(
+        @Body() createUserDto: CreateUserDto,
+    ): Promise<{ message: string; user: User }> {
+        const user = await this.userService.createUser(createUserDto);
+        return {
+            message: 'User created successfully',
+            user,
+        };
+    }
+}
